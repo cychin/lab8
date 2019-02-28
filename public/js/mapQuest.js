7f99360e12e70f3355e81a4a514542eff2e7d343
Chiu Chin <wchin107@gmail.com>
2019-02-28 13:32:45 -0800

    thirdpartAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = '6A4NR0toIodA6hsGjm9Ngc5nXc1Xwf8E';
+	
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.8787, -117.235913],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.8787, -117.235913]).addTo(map);
 }
